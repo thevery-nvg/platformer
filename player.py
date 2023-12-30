ad3f6d2d3407ba62a0e6172e34153c57005d0ddb
box7 <theverynvg@vk.com>
2023-12-30 18:19:14 +0400

    basic animations added

--- player.py
+++ player.py
@@ -5,11 +5,14 @@ from game_variables import *
 class Player():
     def __init__(self, x, y):
         self.images_right = []
+        self.images_left = []
         self.index = 0
         self.counter = 0
         for num in range(1, 5):
             img_right = pygame.image.load(f'img/guy{num}.png')
             img_right = pygame.transform.scale(img_right, (50, 80))
+            img_left = pygame.transform.flip(img_right, True, False)
+            self.images_left.append(img_left)
             self.images_right.append(img_right)
         self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
@@ -17,6 +20,7 @@ class Player():
         self.rect.y = y
         self.vel_y = 0
         self.jumped = False
+        self.direction = 0
 
     def update(self):
         dx = 0
@@ -26,9 +30,11 @@ class Player():
         if key[pygame.K_LEFT]:
             self.counter += 1
             dx -= 5
+            self.direction = -1
         if key[pygame.K_RIGHT]:
             self.counter += 1
             dx += 5
+            self.direction = 1
         if key[pygame.K_SPACE]:
             if not self.jumped:
                 self.vel_y = -15
@@ -47,7 +53,10 @@ class Player():
                 self.index = 0
             else:
                 self.index += 1
-            self.image = self.images_right[self.index]
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
 
         # gravity
         self.vel_y += 1
